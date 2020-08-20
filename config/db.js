0da54cbebb009b652dcde023c66dc4fbbb2019b5
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-20 14:59:48 +0300

    bg:fixing_async_bug:backend

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-
+const colors = require('colors')
 
 const connectDB = async () => {
     try {
-        const conn = mongoose.connect(process.env.MONGO_URI,{
+        const conn = await  mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useCreateIndex:true
         })
-        console.log(`MongoDb connected on :${conn.connection.host}`)
+        console.log(`MongoDb connected on :${conn.connection.host}`.cyan.underline.bold)
     } catch (err){
         console.log(`Error is:${err}`.red.bold)
         process.exit(1)
