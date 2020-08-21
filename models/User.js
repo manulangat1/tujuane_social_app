44b1bb2651712b99af9c6129de6794f2c4300189
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-21 13:19:36 +0300

    rf:modifying_user_model:backend

--- models/User.js
+++ models/User.js
@@ -23,5 +23,28 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('UserSchema',User)
+UserSchema.pre('save', async function(next){
+    const user = this 
+    if (user.isModified('password')){
+        user.password = await bcrypt.hash(user,password,8)
+    }
+})
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+    // Search for a user by email and password.
+    try {
+        const user = await User.findOne({email})
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        await return user
+    } catch (err){
+        console.log(`Error :${err}`)
+        res.status(400).json({
+            success:false,
+            message:'User not found'
+        })
+    }
+}
+
+
+const User = mongoose.model(User,'UserSchema')
 module.exports = User
