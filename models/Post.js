774cccc0fd123234718f28c93316aeea5795ec9b
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-22 15:14:46 +0300

    ft:adding_post_model:backend

---
+++ models/Post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+
+const PostSchema = new mongoose.Schema({
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:User
+    },
+    body:{
+        type:String, 
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model(Post,'PostSchema')
