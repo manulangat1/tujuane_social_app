5ed00d604f433428a6571bb2ad4319ffe4ee5bcb
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-22 15:55:13 +0300

    ft:update_posts:backend

--- controllers/Post.js
+++ controllers/Post.js
@@ -39,3 +39,28 @@ exports.getPosts = async (req,res) => {
         })
     }
 }
+
+exports.updatePost = async (req,res) => {
+    try{
+        const _id = req.params.id
+        const { body} = req.body
+        const post = await Post.findById(_id)
+        if (req.user === post.author){
+            console.log("Hey..")
+        } else {
+            post.body = body 
+        await post.save()
+            res.status(201).json({
+                success:true,
+                data:post
+            })
+        }
+        
+    } catch (err){
+        console.log(`Error is:${err}`)
+        res.status(500).json({
+            success:false,
+            message:'Internal Server Error'
+        })
+    }
+}
--- routes/Post.js
+++ routes/Post.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
-const { addPost , getPosts } = require('../controllers/Post')
+const { addPost , getPosts , updatePost} = require('../controllers/Post')
 
 const { isAuth } = require('../middleware/isAuth')
 const { isActive } = require('../middleware/isActive')
 
 const router = express.Router()
 router.route('/').get(isAuth,isActive,getPosts).post(isAuth,isActive, addPost)
+router.route('/:id/').put(isAuth,isActive,updatePost)
 
 module.exports = router
