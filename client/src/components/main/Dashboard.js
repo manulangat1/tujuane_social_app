884630b0bb69e6ea27366795424ebb3ab46f344e
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-24 12:21:01 +0300

    ft:adding_site:frontend

---
+++ client/src/components/main/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+class Dashboard extends React.Component {
+    render(){
+        return (
+            <main>
+                <p>Hey there</p>
+                </main>
+        )
+    }
+}
+export default Dashboard
